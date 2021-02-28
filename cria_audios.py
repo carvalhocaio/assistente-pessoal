7ccdd9d296d3a158203be52ee5f079b6b4fcfe3c
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-02-27 21:22:01 -0400

    Gravando e reproduzindo áudio

--- cria_audios.py
+++ cria_audios.py
@@ -1,4 +1,15 @@
 from gtts import gTTS
+from playsound import playsound # for Windows
+#from subprocess import call # for OSX and Linux
 
-tts = gTTS('Oi, eu sou a Sexta-Feira', lang='pt-br')
-tts.save('audios/hello.mp3')
+
+def cria_audio(audio):
+    tts = gTTS(audio, lang='pt-br')
+    tts.save('audios/bem_vindo.mp3')
+
+    playsound('audios/bem_vindo.mp3') # for Windows
+    #call(['afplay', 'audios/hello.mp3']) # for OSX
+    #call(['aplay', 'audios/hello.mp3']) # for Linux
+
+
+cria_audio('Olar, eu sou a Isa')
