ed9878f8baf6e1b14b6fffec4c3e0b94d5b3017c
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-03-03 12:46:45 -0400

    SpeechRecogniton

---
+++ isa.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+# obtain audio from the microphone
+microfone = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Aguardando o comando: ")
+    audio = microfone.listen(source)
+
+try:
+    print("Google Speech Recognition thinks you said " + microfone.recognize_google(audio, language='pt-BR'))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print(f"Could not request results from Google Speech Recogniton Speech {e}")
