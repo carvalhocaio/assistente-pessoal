82a4d55108ec6c0e5f7528975a68867730c673e3
carvalhocaio <caiocarvalho.py@gmail.com>
2021-03-04 21:10:43 -0400

    hotword

--- isa.py
+++ isa.py
@@ -1,16 +1,29 @@
 import speech_recognition as sr
 
+hotword = 'isa'
 
-def monitora_microfone():
+def monitora_audio():
     # obtain audio from the microphone
     microphone = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Aguardando o comando: ")
-        audio = microphone.listen(source)
+        while True:
+            print("Aguardando o comando: ")
+            audio = microphone.listen(source)
 
-    try:
-        print(microphone.recognize_google(audio, language='pt-BR'))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recogniton Speech {e}")
+            try:
+                trigger = microphone.recognize_google(audio, language='pt-BR')
+                trigger = trigger.lower()
+
+                if hotword  in trigger:
+                    print('Comando: ', trigger)
+                    ### executar os comandos
+                    break
+                
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print(
+                    f"Could not request results from Google Speech Recogniton Speech {e}")
+
+
+monitora_audio()
